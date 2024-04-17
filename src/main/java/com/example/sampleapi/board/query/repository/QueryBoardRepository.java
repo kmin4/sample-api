a93536d2db596f351e4b4a808fdc9882852a64b3
kmin4 <kmin.kim@osckorea.com>
2024-04-17 08:20:25 +0000

    fix annotation board to count

--- src/main/java/com/example/sampleapi/board/query/repository/QueryBoardRepository.java
+++ src/main/java/com/example/sampleapi/board/query/repository/QueryBoardRepository.java
@@ -21,7 +21,7 @@ public interface QueryBoardRepository extends CrudRepository<BoardDto, Integer> 
     @Query(QueryBoardSqls.BOARD)
     BoardDto  board(int num);
 
-    @Query(QueryBoardSqls.BOARD)
+    @Query(QueryBoardSqls.COUNT)
     String count();
 
 }
